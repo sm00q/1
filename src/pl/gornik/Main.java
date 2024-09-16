47c3a804afbad8ccf689e5c5d2d42353363ab9ed
sm00q <filipplachecki2204@gmail.com>
2024-09-16 12:04:19 +0200

    first commit

---
+++ src/pl/gornik/Main.java
@@ -0,0 +1,18 @@
+package pl.gornik;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.println("Podaj liczbę");
+        int number = scanner1.nextInt();
+        System.out.println(number + " To jest liczba odczytana z klawiatury");
+        System.out.printf("");
+
+        System.out.println(number + " to jest liczba odczytana z klawiatury");
+
+        System.out.printf("%d to jest liczba odczytana z klawiatury\n", number);
+        System.out.println("Tekst");
+    }
+}
